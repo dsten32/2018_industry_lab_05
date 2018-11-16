749453e2759accfd69ba4926921041d871133a6e
Yu-Cheng Tu <yu-cheng.tu@auckland.ac.nz>
2018-11-16 18:20:15 +1300

    Industry Lab 05

---
+++ src/ictgradschool/industry/abstraction/farmmanager/animals/Cow.java
@@ -0,0 +1,34 @@
+package ictgradschool.industry.abstraction.farmmanager.animals;
+
+/**
+ * This class defines a Cow. A cow has an initial value of $1000, and a maximum value of $1500.
+ * The cost to feed a cow is $50.
+ * You may modify this class according to the assignment.
+ *
+ * @author write your name and UPI here.
+ */
+public class Cow extends Animal {
+    private final int MAX_VALUE = 1500;
+
+    public Cow() {
+        value = 1000;
+    }
+
+    public void feed() {
+        if (value < MAX_VALUE) {
+            value += 100;
+        }
+    }
+
+    public int costToFeed() {
+        return 60;
+    }
+
+    public String getType() {
+        return "Cow";
+    }
+
+    public String toString() {
+        return getType() + " - $" + value;
+    }
+}
